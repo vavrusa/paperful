084d03815962c9634823c9d3bd9370a5399dec95
Marek Vavruša <mvavrusa@cloudflare.com>
2019-09-21 19:36:22 -0700

    Initial Commit

---
+++ Paperful/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Paperful
+//
+//  Created by Marek Vavrusa on 9/21/19.
+//  Copyright © 2019 Marek Vavrusa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
